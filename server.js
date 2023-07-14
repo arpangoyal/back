66526c3f7283349c6b256f9bed8a3cca39d54fc3
arpangoyal <arpangoyal2002@gmail.com>
2023-07-14 22:22:32 +0530

    q

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Content-Type': 'application/json"
   );
   next();
 });
